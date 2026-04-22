ea2a6e1d04920f5a01f42e8a9a816aafd2a2e0ac
romain-delorme <261893008+romain-delorme@users.noreply.github.com>
2026-04-22 11:04:55 +0200

    added api auth

--- galene-api-wrapper/api.py
+++ galene-api-wrapper/api.py
@@ -1,24 +1,28 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 class GaleneAPI():
     '''An API wrapper for Galène'''
 
-    username = ""
-    password = ""
+    load_dotenv()
 
-    def __init__(self, url="localhost", location="/galene-api/v0/"):
+    username = os.getenv('API_ADMIN_LOGIN')
+    password = os.getenv('API_ADMIN_PASSWORD')
+
+    def __init__(self, url="http://localhost", location="/galene-api/v0/"):
         '''Initialize GaleneAPI instance with server url (default is localhost) 
         and API endpoints path (default is /galene-api/v0/, as per the official Galène docs)'''
         self.url = url
         self.location = location
         self.api_path = url + location
 
-    def getstats():
+    def getstats(self):
         '''Gets stats, returns a JSON (TODO: explain further)'''
-        req = requests.get(self.api_path + ".stats", auth=(username, password))
+        req = requests.get(self.api_path + ".stats", auth=(self.username, self.password))
         return req.json()
 
-    def list_groups():
-        '''Lists groups, returns a JSON (TODO: explain further)'''
+    def list_groups(self):
+        '''The .groups galene API endpoint exists in docs but seems to return a 404 here, idk why'''
         req = requests.get(self.api_path + ".groups")
         return req.json()
--- galene-api-wrapper/main.py
+++ galene-api-wrapper/main.py
@@ -1,5 +1,14 @@
+import sys
+import pathlib
+
+sys.path.append(str(pathlib.Path(__file__).parent.resolve()))
+
+from api import GaleneAPI
+
 def main():
     print("Hello from galene-api-wrapper!")
+    api = GaleneAPI(url="https://dty-s26-p2-galene.k8s-cloud.centralesupelec.fr/")
+    print(api.list_groups())
 
 
 if __name__ == "__main__":
