3f3df22e05323a4d244edd2d373baa04ee7a45de
Romain DTY <romaindty@Romains-MacBook-Air.local>
2026-04-23 14:24:41 +0200

    création du package pour appeler le sdk

---
+++ galene-api/galene/api/__init__.py
@@ -0,0 +1,13 @@
+from .galene_api import GaleneAPI
+from .models import GroupDefinition, UserDefinition, StatefulToken
+from .access_token import AccessToken, VideoGrants, TokenVerifier
+
+__all__ = [
+    "GaleneAPI",
+    "GroupDefinition",
+    "UserDefinition",
+    "StatefulToken",
+    "AccessToken",
+    "VideoGrants",
+    "TokenVerifier",
+]
---
+++ galene-rtc/galene/rtc/__init__.py
@@ -0,0 +1,5 @@
+from .signal_client import SignalClient
+
+__all__ = [
+    "SignalClient",
+]
