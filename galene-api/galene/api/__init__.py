6f4ad2c987eb7748e5a23ad3fd1b6b0b771fe20c
Paloys <40764414+Paloys@users.noreply.github.com>
2026-04-29 14:53:30 +0200

    Added GaleneError to __init__.py

--- galene-api/galene/api/__init__.py
+++ galene-api/galene/api/__init__.py
@@ -1,6 +1,7 @@
 from .galene_api import GaleneAPI
 from .models import GroupDefinition, UserDefinition, StatefulToken
 from .access_token import AccessToken, VideoGrants, TokenVerifier
+from .exceptions import GaleneError
 
 __all__ = [
     "GaleneAPI",
